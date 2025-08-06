af9ecc8057df771a2ff9ce0afca48bb2fd777b34
Rhe754Tt <slates.zapper_0k@icloud.com>
2025-08-06 12:54:08 +0900

    Add 防止SQL注入

---
+++ prevent_sql_injection_0806_1154_pum.go
@@ -0,0 +1,13 @@
+// 代码生成时间: 2025-08-06 11:54:08
+package main
+
+import (
+    "database/sql"
+    "fmt"
+    "log"
+    "net/http"
+    "strings"
+    "time"
+
+    _ "github.com/go-sql-driver/mysql" // mysql驱动
+    
